2c5c340d6ea4a525db5ccada1e313b16803299d9
weichangyao <wyao1@stuy.edu>
2019-02-28 11:14:00 -0500

    added setAnimate and claer

--- Maze.java
+++ Maze.java
@@ -7,9 +7,20 @@ public class Maze{
       File f = new File(fileName);
       Scanner in = new Scanner(f);
       char[][] scanFile = new char[][];
-      for (int i = 0; i < c)
+      for (int i = 0; i < c; i++){
+
+      }
       return scanFile;
     }
+    public void setAnimate(boolean b){
+      animate = b;
+    }
+    public void clearTerminal(){
+      System.out.println("\033[2J\033[1;1H");
+    }
+
+
+
     public static void main(String args[]){
       char[][] a = new char[][];
       a = getFile("Maze1.txt");
